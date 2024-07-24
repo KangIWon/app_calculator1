79d5d7f71356012acb5ce78be56c23b5aaa13c73
KangIWon <esfne4727@naver.com>
2024-07-24 16:30:01 +0900

    배열 선언 및 생성, 연산 결과 저장, 10개를 초과하는 경우 저장할 수 있게 소스 코드 수정 그런데 뭔가 잘 안됨. 카운트 세고 10개가 초과하면 결과값이 저장이 되어야하는데 되긴 하는데 바로 안뜨고 한 번 더 연산을 해야만 확인이 가능함. 왜 그런지 모르겠음. 연산값이 없어지진 않는 것 같음. 그런데 바로 확인은 안됨...

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,7 +5,9 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //String answer;
+        int[] intArray = new int[10];    // 정수 배열
+        int cnt = 0; //카운트
+
         String answer;
         do {
             System.out.print("첫 번째 숫자를 입력하세요: ");
@@ -37,6 +39,21 @@ public class App {
             }
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
             System.out.println("결과: " + result);
+
+            if (cnt<=9){
+                intArray[cnt] = result;
+                cnt++;
+            } else if (cnt>9) {
+                for (int i=0;i<9;i++){
+                    intArray[i] = intArray[i+1];
+                }
+                intArray[9]= result;
+            }
+
+            for (int i=0;i<9;i++){
+                System.out.println(intArray[i]);
+            }
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             answer = sc.next();
             if (answer.equals("exit")) {
