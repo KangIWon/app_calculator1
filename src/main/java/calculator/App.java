574da782201dee1a9a6cdc6837afc486949a9872
KangIWon <esfne4727@naver.com>
2024-07-24 14:12:52 +0900

    오류 메시지 출력. exit 다시 설정.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String answer;
+        //String answer;
         do {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
@@ -29,12 +29,18 @@ public class App {
                 case '/':
                     result = num1 / num2;
                     break;
+                default:
+                    // 오류 메시지 출력
+                    System.out.println("기호 입력이 잘못됬습니다."); break;
             }
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            answer = sc.nextLine();
-        } while (answer != "exit");
+            String answer = sc.nextLine();
+            if (answer.equals("exit")) {
+                break;
+            }
+        } while (true);
 
 
     }
