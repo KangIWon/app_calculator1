07aadf8aec47930d968732bace63a0d77129d6f9
KangIWon <esfne4727@naver.com>
2024-07-24 11:54:00 +0900

    2숫자 입력

---
+++ src/main/java/calculator/App.java
@@ -0,0 +1,17 @@
+package calculator;
+import java.util.Scanner;
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+
+    }
+}
