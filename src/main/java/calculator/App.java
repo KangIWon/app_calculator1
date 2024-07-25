cf684b290cfcc6ea045da67c582837811ce361a0
KangIWon <esfne4727@naver.com>
2024-07-25 19:15:00 +0900

    레벨2-7 static, final 활용해서 반지름 상수로 설정. 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성. 생성자 수정. 원의 넓이를 구하는 메서드 선언.원의 넓이 저장 필드 Getter, Setter, 조회 메서드 구현.(사칙연산 코드를 수정해서 사용).Calculator 인스턴스 수정. 사칙연산을 진행할지 원의 너비를 구할지 선택 구현. 원 넓이 구하는 거 사칙연산 코드 활용해서 수정.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,39 +5,62 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator(new ArrayList<>()); //수정()안에 ArrayList가 들어가게
+        Calculator calculator = new Calculator(new ArrayList<>(), new ArrayList<>()); //수정()안에 ArrayList가 들어가게
 
         Scanner sc = new Scanner(System.in);
 
         String answer;
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = sc.nextInt();
+            /* 사칙연산을 진행할지 원의 너비를 구할지 선택 구현 */
+            System.out.print("1. 사칙 연산을 진행하시겠습니까? / 2. 원의 너비를 구하시겠습니까? :");
+            int num = sc.nextInt();
+            if (num==1) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = sc.nextInt();
 
-            System.out.print("사칙연산 기호를 입력하세요: "); //+, -, *, /
-            char operator = sc.next().charAt(0);
+                System.out.print("사칙연산 기호를 입력하세요: "); //+, -, *, /
+                char operator = sc.next().charAt(0);
 
-            int result = calculator.calculate(num1, num2, operator);
-            calculator.getResults().add(result); //결과값 추가 getResults로 바꿈
+                int result = calculator.calculate(num1, num2, operator);
+                calculator.getResults().add(result); //결과값 추가 getResults로 바꿈
 
-            System.out.println(calculator.getResults().toString()); //출력 getResults로 바꿈
+                System.out.println(calculator.getResults().toString()); //출력 getResults로 바꿈
 
 
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            answer = sc.next();
-            if (answer.equals("remove")) {
-                calculator.removeResult(0); //삭제 removeResult로 바꿈
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하였습니다");
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                answer = sc.next();
+                if (answer.equals("remove")) {
+                    calculator.removeResult(0); //삭제 removeResult로 바꿈
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하였습니다");
+                }
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                answer = sc.next();
+                if (answer.equals("inquiry")) {
+                    calculator.inquiryResults(); // inquiryResults로 출력
+                }
+            } else if (num==2) {
+                /* 원의 넓이를 구하는 경우 반지름을 입력받아 원의 넓이를 구한 후 출력*/
+                System.out.print("반지름을 입력하세요: ");
+                int r = sc.nextInt();
+                double circleResults = calculator.calculateCircleArea(r);
+                /* 원의 넓이 저장 */
+                calculator.getcircleResults().add(circleResults); //결과값 추가
+
+                System.out.println(calculator.getcircleResults().toString()); //출력
+
+                /* 저장된 원의 넓이 값들 바로 전체 조회 */
+                System.out.println("저장된 원의 넓이를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                answer = sc.next();
+                if (answer.equals("inquiry")) {
+                    calculator.inquirycircleResults(); // inquirycircleResults로 출력
+                }
             }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            answer = sc.next();
-            if (answer.equals("inquiry")) {
-                calculator.inquiryResults(); // inquiryResults로 출력
+            else {
+                System.out.println("정확한 숫자를 선택해주세요. (1과 2 중에 하나를 선택해주세요.)");
             }
-
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             answer = sc.next();
             if (answer.equals("exit")) {
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,9 +5,14 @@ public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드가 생성자를 통해 초기화 되도록 변경 */
     private ArrayList<Integer> results;
 
-    /* 생성자 구현 */
-    public Calculator(ArrayList<Integer> results) {
+    /* static, final 활용 */
+    private static final double pi = 3.14; // 파이를 상수로 선언 파이값은 바뀌지 않으니까.
+    /* 원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성 */
+    private ArrayList<Double> circleResults; // 원 넓이 결과 저장
+    /* 생성자 수정 */
+    public Calculator(ArrayList<Integer> results, ArrayList<Double> circleResults) {
         this.results = results;
+        this.circleResults = circleResults;
         // 생성자를 선언하는데 매개변수명과 객체의 필드명이 동일하므로 this를 써줌.
     }
 
@@ -42,6 +47,26 @@ public class Calculator {
 
         return result;
         }
+    public double calculateCircleArea(int r) { // 반지름 r로 설정
+        /* 원의 넓이 계산 구현 */
+        return pi * r * r;
+    }
+    /* 원의 넓이 저장 필드 Getter, Setter, 조회 메서드 구현 */
+    public ArrayList<Double> getcircleResults() { // getcircleResults circleResults값 가져오기
+        return circleResults;
+    }
+
+    public void setcircleResults(ArrayList<Double> circleResults) { // setcircleResults circleResults값 수정하기
+        this.circleResults = circleResults;
+    }
+
+    public void inquirycircleResults() { // inquirycircleResults 결과 조회
+        /* 구현 */
+        for (double n: getcircleResults()){
+            System.out.println("원 넓이 계산 결과 : "+ n + " "); // 줄바꿈이 있게 나올 수 있도록 함
+        }
+        System.out.println(getcircleResults().toString()); // 연산 결과 circleResults에 잘 들어가있는지 한 번 더 확인
+    }
 
     /* Getter 메서드 구현 */
     /* Setter 메서드 구현 */
