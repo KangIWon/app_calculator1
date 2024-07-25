bfd4ec9230e213fa49e9c86c2891fdbe79071538
KangIWon <esfne4727@naver.com>
2024-07-25 11:12:24 +0900

    레벨2 - 1 그런데 throw가 안됨. 어떻게 써야하는지 잘 모르겠음. 주석처리해놓음.

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 public class App {
 
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+//        calculator.calculate();
+
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Integer> intList = new ArrayList<Integer>(); // 선언 및 생성
@@ -18,31 +21,31 @@ public class App {
 
             System.out.print("사칙연산 기호를 입력하세요: "); //+, -, *, /
             char operator = sc.next().charAt(0);
-
-            int result = 0;
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    result = num1 / num2;
-                    break;
-                default:
-                    // 오류 메시지 출력
-                    System.out.println("기호 입력이 잘못됬습니다.");
-                    break;
-            }
-            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-            System.out.println("결과: " + result);
-
-
-            intList.add(result); //결과값 추가
+//
+//            int result = 0;
+//            switch (operator) {
+//                case '+':
+//                    result = num1 + num2;
+//                    break;
+//                case '-':
+//                    result = num1 - num2;
+//                    break;
+//                case '*':
+//                    result = num1 * num2;
+//                    break;
+//                case '/':
+//                    result = num1 / num2;
+//                    break;
+//                default:
+//                    // 오류 메시지 출력
+//                    System.out.println("기호 입력이 잘못됬습니다.");
+//                    break;
+//            }
+//            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+//            System.out.println("결과: " + result);
+//
+//
+//            intList.add(result); //결과값 추가
 
             System.out.println(intList.toString()); //출력
 
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,44 @@
 package calculator;
+import java.util.ArrayList;
 
 public class Calculator {
 
+    double result = 0;
+    public double calculate(double num1, double num2, char operator) {
+        ArrayList<Double> intList = new ArrayList<>(); // 선언 및 생성
+
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    //throw new Exception("나눗셈에서 분모에 0이 입력될 수 없습니다.");
+                }
+                break;
+            default:
+                // 오류 메시지 출력
+                System.out.println("기호 입력이 잘못됬습니다.");
+                //throw new Exception("기호 입력이 잘못됬습니다.");
+                break;
+        }
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+        System.out.println("결과: " + result);
+
+
+        intList.add(result); //결과값 추가
+
+        System.out.println(intList.toString()); //출력
+
+        return result;
+    }
 }
