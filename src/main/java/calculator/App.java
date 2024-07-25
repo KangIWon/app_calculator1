4269d5ce0dcc5f15f48456e065fb18a23a6bb565
KangIWon <esfne4727@naver.com>
2024-07-25 14:52:20 +0900

    레벨2 - 1 그런데 throw가 안됨. 어떻게 써야하는지 잘 모르겠음. 주석처리해놓음. 조금 수정함 double->int로 바꿔봄

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,7 +9,7 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        ArrayList<Integer> intList = new ArrayList<Integer>(); // 선언 및 생성
+//        ArrayList<Double> intList = new ArrayList<Double>(); // 선언 및 생성
         int cnt = 0; //카운트
 
         String answer;
@@ -21,6 +21,9 @@ public class App {
 
             System.out.print("사칙연산 기호를 입력하세요: "); //+, -, *, /
             char operator = sc.next().charAt(0);
+
+            int result = calculator.calculate(num1, num2, operator);
+            //calculator.results.add(result);
 //
 //            int result = 0;
 //            switch (operator) {
@@ -47,24 +50,24 @@ public class App {
 //
 //            intList.add(result); //결과값 추가
 
-            System.out.println(intList.toString()); //출력
+            System.out.println(calculator.results.toString()); //출력
 
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             answer = sc.next();
             if (answer.equals("remove")) {
-                intList.remove(0); //삭제
+                calculator.results.remove(0); //삭제
                 System.out.println("가장 먼저 저장된 연산 결과를 삭제하였습니다");
-                System.out.println(intList.toString()); //확인 출력
+                System.out.println(calculator.results.toString()); //확인 출력
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             answer = sc.next();
             if (answer.equals("inquiry")) {
-                for (int number:intList){
+                for (int number:calculator.results){
                     System.out.print(number + " ");
                 }
-                System.out.println(intList.toString()); //출력
+                System.out.println(calculator.results.toString()); //출력
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -2,12 +2,12 @@ package calculator;
 import java.util.ArrayList;
 
 public class Calculator {
-
+    ArrayList<Integer> results = new ArrayList<>();
     double result = 0;
-    public double calculate(double num1, double num2, char operator) {
-        ArrayList<Double> intList = new ArrayList<>(); // 선언 및 생성
+    public int calculate(int num1, int num2, char operator) {
 
-        double result = 0;
+        int result = 0;
+
         switch (operator) {
             case '+':
                 result = num1 + num2;
@@ -22,7 +22,8 @@ public class Calculator {
                 if (num2 != 0) {
                     result = num1 / num2;
                 } else {
-                    //throw new Exception("나눗셈에서 분모에 0이 입력될 수 없습니다.");
+                    throw new ArithmeticException("나눗셈에서 분모에 0이 입력될 수 없습니다.");
+                    //ArithmeticException는 나눗셈에서 분모에 0이 입력될 수 없음을 나타내는 예외처리
                 }
                 break;
             default:
@@ -31,14 +32,15 @@ public class Calculator {
                 //throw new Exception("기호 입력이 잘못됬습니다.");
                 break;
         }
+
         /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
         System.out.println("결과: " + result);
 
 
-        intList.add(result); //결과값 추가
+        results.add(result); //결과값 추가
 
-        System.out.println(intList.toString()); //출력
+        System.out.println(results.toString()); //출력
 
         return result;
+        }
     }
-}
